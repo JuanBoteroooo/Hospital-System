dc33cdc4e5e81ca2c6e650f6978e79040e97e553
Juan Botero <110292405+JuanBoteroooo@users.noreply.github.com>
2024-10-17 23:51:19 -0400

    Register done

--- backend/Server.js
+++ backend/Server.js
@@ -6,7 +6,6 @@ var app = express();
 //********************** IMPORTACION DE LOS QUERYS ***********************
 const cors_config = require('../backend/config/cors.json');
 const querys = require('../backend/data/querys.json');
-const models = new (require('../backend/models/userModels.js'))();
 
 //********************** CONFIGURACIONES DEL APP ***********************
 app.use(cors(cors_config));
@@ -65,29 +64,36 @@ app.post('/login', async function (req, res) {
 
 
 app.post('/register', async function (req, res) {
-  const { username, password, email } = req.body;
-
-  if (!username || !password || !email) {
+  const { username, password, name, lastName, phone, email, address } = req.body;
+  if (!username || !password || !name || !lastName || !phone || !email || !address) {
       return res.status(400).send(`{"msg": "datos invalidos..!"}`);
   }
 
   try {
-      // Primero, verifica si el usuario ya existe
-      const userExists = await db.exe('checkUserExists', [username]);
-      if (userExists.rows.length > 0) {
-          return res.status(409).send(`{"msg": "usuario ya registrado..!"}`);
+      // Verifica si el usuario ya existe en la base de datos
+      const existsResult = await db.exe('checkUserExists', [username]);
+      if (existsResult.rows.length > 0) {
+          return res.status(409).send(`{"msg": "usuario ya existente..!"}`);  // 409 Conflict puede ser un código adecuado aquí
       }
 
-      // Si no existe, registra al nuevo usuario
-      const newUser = await db.exe('registerUser', [username, password, email]);
-      if (newUser.rows.length > 0) {
-          return res.send(`{"msg": "usuario registrado..!"}`);
+      // Registra los datos personales si el usuario no existe
+      const personResult = await db.exe('registerData', [name, lastName, phone, email, address]);
+      if (personResult.rows.length > 0) {
+          const personId = personResult.rows[0].person_id; // Asegúrate de que este campo se llama 'person_id'
+
+          // Luego, registra el usuario con el person_id obtenido
+          const userResult = await db.exe('registerUser', [username, password, personId]);
+          if (userResult.rows.length > 0) {
+              return res.send(`{"msg": "usuario registrado con éxito..!"}`);
+          } else {
+              return res.status(400).send(`{"msg": "no se pudo registrar el usuario..!"}`);
+          }
       } else {
-          return res.status(400).send(`{"msg": "no se pudo registrar el usuario..!"}`);
+          return res.status(400).send(`{"msg": "no se pudo registrar los datos personales..!"}`);
       }
   } catch (error) {
       console.error('Registration error:', error);
-      res.status(500).send(`{"msg": "Internal server error..!"}`);
+      return res.status(500).send(`{"msg": "Internal server error..!"}`);
   }
 });
 
