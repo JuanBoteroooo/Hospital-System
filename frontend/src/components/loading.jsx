1b5216ef8077ab236a6ed07caba58444dfb10feb
Juan Botero <110292405+JuanBoteroooo@users.noreply.github.com>
2024-10-28 17:56:36 -0400

    Home done

--- frontend/src/components/loading.jsx
+++ frontend/src/components/loading.jsx
@@ -6,7 +6,7 @@ const HeartLoader = () => {
     return (
         <div className="loading-container">
             {/* <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" enableBackground="new 0 0 24 24" xmlSpace="preserve" className="heart-svg">
-                <path fill="#3e5b97" d="M12,21.35l-1.45-1.32C5.4,15.36,2,12.28,2,8.5C2,5.42,4.42,3,7.5,3c1.74,0,3.41,0.81,4.5,2.09 C13.09,3.81,14.76,3,16.5,3C19.58,3,22,5.42,22,8.5c0,3.78-3.4,6.86-8.55,11.54L12,21.35z" />
+                <path fill="#FF0000" d="M12,21.35l-1.45-1.32C5.4,15.36,2,12.28,2,8.5C2,5.42,4.42,3,7.5,3c1.74,0,3.41,0.81,4.5,2.09 C13.09,3.81,14.76,3,16.5,3C19.58,3,22,5.42,22,8.5c0,3.78-3.4,6.86-8.55,11.54L12,21.35z" />
             </svg> */}
             <img src={corazon} alt="cora" className="heart-svg" />
         </div>
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import NavBar from "../components/navbar";
-import Card from "../components/card";
 import Loading from "../components/loading"; // Importa el componente de carga
+import f1 from "../assets/images/agendar.jpg";
+import f2 from "../assets/images/hospitalizacion.jpg";
+import f3 from "../assets/images/tratamiento.jpg";
+import f4 from "../assets/images/historia.jpg";
+import f5 from "../assets/images/examen.jpg";
 
 const Home = () => {
     const [loading, setLoading] = useState(true); // Controla el estado de carga
@@ -14,7 +18,7 @@ const Home = () => {
         const simulateLoading = setTimeout(() => {
             // Aquí podrías también verificar la sesión como lo hacías antes
             setLoading(false); // Cambia a falso después del tiempo simulado
-        }, 2000); // 2 segundos de "carga ficticia"
+        }, 3000); // 2 segundos de "carga ficticia"
 
         return () => clearTimeout(simulateLoading); // Limpiar timeout si el componente se desmonta
     }, []);
@@ -25,15 +29,36 @@ const Home = () => {
 
     return (
         <div className="home-page">
-            <NavBar />
+            <div className="header">
+                <NavBar />
+            </div>
             <div className="home-container">
                 <h1>Tu salud es primero</h1>
             </div>
-            <div className="card-wrap">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+            <div className="img-carousel">
+                <div className="linewhite"></div>
+                <section>
+                    <div>
+                        <img src={f1} alt="Agendar" />
+                        <label>Citas</label>
+                    </div>
+                    <div>
+                        <img src={f4} alt="Historia clínica" />
+                        <label>Historia clínica</label>
+                    </div>
+                    <div>
+                        <img src={f5} alt="Exámenes" />
+                        <label>Exámenes</label>
+                    </div>
+                    <div>
+                        <img src={f3} alt="Tratamiento" />
+                        <label>Tratamiento</label>
+                    </div>
+                    <div>
+                        <img src={f2} alt="Hospitalización" />
+                        <label>Hospitalización</label>
+                    </div>
+                </section>
             </div>
         </div>
     );
